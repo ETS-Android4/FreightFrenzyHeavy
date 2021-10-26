9d07c95d6607fa7f46e4a47110dbf1bb6c8c702e
Emmett Weisz <48953784+emmett-w@users.noreply.github.com>
2021-10-26 07:47:38 -0400

    Add files from Light repo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -0,0 +1,131 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class Hardware extends LinearOpMode {
+
+
+    // Good Luck!
+    //You should put constants here
+
+    protected DcMotor frontLeft, frontRight, backLeft, backRight, clawStrafe, clawRotate ;
+    protected Servo clawGrab;
+
+    static final double     COUNTS_PER_MOTOR_REV    = 420 ;    // Needs to be fixed based on the motors
+    static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference. Not sure what it is
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+
+    public ElapsedTime runtime = new ElapsedTime();
+
+
+    // Setup your drivetrain (Define your motors etc.)
+    public void hardwareSetup() {
+
+
+
+        // Define your methods of driving (Encoder drive, tank drive, etc.
+
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        backRight = hardwareMap.dcMotor.get("backRight");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        clawStrafe = hardwareMap.dcMotor.get("clawStrafe");
+        clawRotate = hardwareMap.dcMotor.get("clawRotate");
+        clawGrab = hardwareMap.servo.get("clawGrab");
+
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //Use encoders
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        telemetry.addData("Status:", "Setup Complete");
+        telemetry.update();
+    }
+    // Pinchas should make an encoder drive
+    public int encoderDrive(double maxPower, double frontRightInches, double frontLeftInches, double backLeftInches, double backRightInches){
+        // stop and reset the encoders? Maybe not. Might want to get position and add from there
+        double newFRTarget;
+        double newFLTarget;
+        double newBLTarget;
+        double newBRTarget;
+
+        if (opModeIsActive()){
+            //calculate and set target positions
+
+            newFRTarget = frontRight.getCurrentPosition()     +  (frontRightInches * COUNTS_PER_INCH);
+            newFLTarget = frontLeft.getCurrentPosition()     +  (frontLeftInches * COUNTS_PER_INCH);
+            newBLTarget = backLeft.getCurrentPosition()     +  (backLeftInches * COUNTS_PER_INCH);
+            newBRTarget = backRight.getCurrentPosition()     + (backRightInches * COUNTS_PER_INCH);
+
+            backRight.setTargetPosition((int)(backRightInches));
+            frontRight.setTargetPosition((int)(frontRightInches));
+            frontLeft.setTargetPosition((int)(frontLeftInches));
+            backLeft.setTargetPosition((int)(backLeftInches));
+
+            // Run to position
+            frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+            // Set powers. For now I'm setting to maxPower, so be careful.
+            // In the future I'd like to add some acceleration control through powers, which
+            // should help with encoder accuracy. Stay tuned.
+            runtime.reset();
+            frontRight.setPower(maxPower);
+            frontLeft.setPower(maxPower);
+            backRight.setPower(maxPower);
+            backLeft.setPower(maxPower);
+
+            //
+
+            while (opModeIsActive() &&
+                    (frontRight.isBusy() && frontLeft.isBusy() && backRight.isBusy() && backLeft.isBusy() )) {
+                    // Do nothing
+
+            }
+            // Set Zero Power
+            frontRight.setPower(0);
+            frontLeft.setPower(0);
+            backRight.setPower(0);
+            backLeft.setPower(0);
+
+            // Go back to Run_Using_Encoder
+            frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+
+
+
+        }
+
+
+        return 0;
+    }
+    // Last thing is an empty runOpMode because it's a linearopmode
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
