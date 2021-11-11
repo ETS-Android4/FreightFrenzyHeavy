85507ba3f4735ffd5b14c383e2efea1fb49328f8
YUHSB Lionotics <lionotics@yuhsbstudents.org>
2021-11-11 12:36:58 -0500

    Added auto
    Drives forward then back

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -1,15 +1,29 @@
 package org.firstinspires.ftc.teamcode;
 
 public class Auto extends Hardware {
+    // Fake values for now. Just showing how to call it
+    TensorflowDetector recongnizer = new TensorflowDetector(10,20, 1);
+
     @Override
     public void runOpMode(){
-        // Fake values for now. Just showing how to call it
-        TensorflowDetector recongnizer = new TensorflowDetector(10,20, 1);
-
+        //Prep for tensorflow:
         int place = recongnizer.recognizeObjects();
-
         // Then move to the place.
 
+        //Drive forward
+        telemetry.addData("Status", "Driving Forward");
+        telemetry.update();
+        driveForward(0.5);
+        sleep(1000);
+
+        driveForward(0);
+        sleep(200);
+
+        telemetry.addData("Status","Driving Backward");
+        telemetry.update();
+        driveForward(-0.5);
+        sleep(1000);
+        driveForward(0);
     }
 
 }
